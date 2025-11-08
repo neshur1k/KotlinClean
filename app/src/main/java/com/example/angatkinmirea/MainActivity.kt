0e07af1a37990b5296b39de0a813d43f47fb642c
Alexander <alexander.angatkin@gmail.com>
2025-11-08 21:31:08 +0300

    task 2

--- app/src/main/java/com/example/angatkinmirea/MainActivity.kt
+++ app/src/main/java/com/example/angatkinmirea/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.tooling.preview.devices.WearDevices
 import com.example.angatkinmirea.ui.theme.AngatkinMIREATheme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +49,40 @@ fun Hello(name: String?, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    name = "portrait", showBackground = true, showSystemUi = true,
+    widthDp = 400, heightDp = 800
+)
 @Composable
-fun HelloPreview() {
+fun HelloPreviewPortrait() {
     AngatkinMIREATheme {
         Hello("Александр")
+    }
+}
+
+@Preview(
+    name = "landscape", showBackground = true, showSystemUi = true,
+    widthDp = 800, heightDp = 400
+)
+@Composable
+fun HelloPreviewLandscape() {
+    AngatkinMIREATheme {
+        Hello("Александр")
+    }
+}
+
+@Preview(
+    name = "round", showBackground = true, showSystemUi = true,
+    widthDp = 200, heightDp = 200, backgroundColor = 0xFFFFFF00
+)
+@Composable
+fun HelloPreviewRound() {
+    AngatkinMIREATheme {
+        Box(
+            modifier = Modifier.size(200.dp).clip(CircleShape).background(Color.Yellow),
+            contentAlignment = Alignment.Center
+        ) {
+            Hello("Александр")
+        }
     }
 }
