e504bbd4c5eaeb0330faf4b63dddf845871bbde5
Alexander <alexander.angatkin@gmail.com>
2025-12-09 00:28:45 +0300

    task 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.wear.tooling.preview)
+    implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.ui.test.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/angatkinmirea/ui/theme/Type.kt
+++ app/src/main/java/com/example/angatkinmirea/ui/theme/Type.kt
@@ -2,33 +2,53 @@ package com.example.angatkinmirea.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.googlefonts.Font
+import com.example.angatkinmirea.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
 )
+
+val bodyFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Orbit"),
+        fontProvider = provider,
+    )
+)
+
+val displayFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Luckiest Guy"),
+        fontProvider = provider,
+    )
+)
+
+// Default Material 3 typography values
+val baseline = Typography()
+
+val AppTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.sciencegothic_bold)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 57.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.sciencegothic_medium)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.sciencegothic_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
+
